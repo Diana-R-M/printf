397a92e7744c30b556cb670352281d5082875a91
mmwihaki <marymwihaki16@gmail.com>
2023-06-27 08:59:48 -0700

    update c handler

--- main.h
+++ main.h
@@ -7,7 +7,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int write_char(char a, char buffer_array[], int s,  int p, int f, int w);
 int print_percent(va_list args);
-int char_printf(va_list args, char buffer_array[], int s,  int p, int f, int w);
+int char_printf(va_list args);
 int print_int(va_list args);
 int string_printf(va_list args);
 
