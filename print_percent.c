31e676984c1b3fd5c7b3a5622e76db6bde8f6e50
mmwihaki <marymwihaki16@gmail.com>
2023-06-27 07:38:26 -0700

    % handler

---
+++ print_percent.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_percent - handle % conversion specifiers
+ * @args - arguments
+ * description - handle %
+ * Return: 1
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
