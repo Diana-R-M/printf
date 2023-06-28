2826a4f59a81b0563c9527e868d477fc86e64e48
Diana-R-M <diana@github.com>
2023-06-27 18:41:04 -0700

    Updated prototypes

--- main.h
+++ main.h
@@ -5,7 +5,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int write_char(char a, char buffer_array[], int s,  int p, int f, int w);
 int print_percent(va_list args);
 int char_printf(va_list args);
 int print_int(va_list args);
